588fb0dab5f923af0e8f14146a8ce8681d6abba1
Lucas Moreira <nirvana.lucasmoreira@gmail.com>
2020-04-08 17:34:24 -0300

    :construction: Insere classe TabuleiroException.cs

--- xadrez-console/xadrez-console/tabuleiro/TabuleiroException.cs
+++ xadrez-console/xadrez-console/tabuleiro/TabuleiroException.cs
@@ -4,7 +4,7 @@ namespace tabuleiro
 {
     class TabuleiroException : Exception
     {
-        public TabuleiroException(string msg) : base(msg)
+        public TabuleiroException(string msg) : base(msg) 
         {
 
         }
